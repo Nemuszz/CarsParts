b8816ec7c339c72bfee5f659e11e01b3374e96e8
Nemuszz <nsavic3011@gmail.com>
2024-05-14 17:36:55 +0200

    connected routes

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,16 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('Guest/welcome');
 });
+Route::get('/parts', function () {
+    return view('Guest/parts');
+});
+Route::get('/register', function () {
+    return view('Registration/register');
+});
+Route::get('/login', function () {
+    return view('Registration/login');
+});
 
-Route::get('/proba', function () {
+Route::get('/cars', function () {
     return view('Guest/proba');
 });
