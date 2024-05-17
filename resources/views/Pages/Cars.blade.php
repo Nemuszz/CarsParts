9962bbf8cbc41b523818808302ce933c15f8c540
Nemuszz <nsavic3011@gmail.com>
2024-05-17 16:03:01 +0200

    Car blade restyle

--- resources/views/Pages/Cars.blade.php
+++ resources/views/Pages/Cars.blade.php
@@ -1,6 +1,6 @@
 @include('Layouts.nav')
 <div class=" mx-auto bg-white p-8 rounded-lg shadow-md">
-    <div class="flex h-screen">
+    <div class="flex h-auto">
         <!-- Sidebar -->
         <div class="bg-gray-800 text-white w-64 h-50 flex flex-col">
             <div class="p-4 flex justify-between items-center">
@@ -34,16 +34,15 @@
                         </div>
                         <button type="button" id="addImageInput">Add Image</button>
                         <div class="flex flex-col space-y-2 mx-4">
-                            <label class="text-gray-400">Power</label>
-                        <select required name="make" class="px-4 py-2 rounded-lg border-none bg-gray-300 text-gray-800 focus:outline-none focus:bg-white focus:ring focus:ring-blue-500">
-                            <option value="">Select Make</option>
-                            <option value="Audi">Audi</option>
-                            <option value="Bmw">Bmw</option>
-                            <option value="Mercedes">Mercedes</option>
-                            <!-- Add more options for make -->
-                        </select>
+                            <label class="text-black-400">Make</label>
+                            <select required name="make" class="px-4 py-2 rounded-lg border-none bg-gray-300 text-gray-800 focus:outline-none focus:bg-white focus:ring focus:ring-blue-500">
+                                <option value="Audi">Audi</option>
+                                <option value="Bmw">Bmw</option>
+                                <option value="Mercedes">Mercedes</option>
+                                <!-- Add more options for make -->
+                            </select>
                         </div>
-                        <div>
+                        <div class="flex flex-col space-y-2 mx-4">
                             <label for="model" class="">Model</label>
                             <input id="model" name="model" type="text"  required class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Email address" value="">
                         </div>
@@ -51,7 +50,7 @@
                             <h3 class="mt-6 text-center text-3xl font-extrabold text-gray-900">Description</h3>
                         </div>
                         <div class="flex flex-col space-y-2 mx-4">
-                            <label class="text-gray-400">Year</label>
+                            <label class="text-black-400">Year</label>
                             <select required name="year" class="px-4 py-2 rounded-lg border-none bg-gray-300 text-gray-800 focus:outline-none focus:bg-white focus:ring focus:ring-blue-500">
                                 <option value="">Select Year</option>
                                 <option value="{{$year}}">{{$year}}</option>
@@ -61,25 +60,25 @@
 
                             </select>
                         </div>
-                        <div>
+                        <div class="flex flex-col space-y-2 mx-4">
                             <label for="mileage" class="">Mileage</label>
                             <input id="mileage" name="mileage" type="number" class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Mileage" value="">
                         </div>
-                        <div>
+                        <div class="flex flex-col space-y-2 mx-4">
                             <label for="price" class="">Price</label>
                             <input id="price" name="price" type="number" class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Price in euros" value="">
                         </div>
                         <div class="flex flex-col space-y-2 mx-4">
-                            <label class="text-gray-400">Body type</label>
-                        <select required name="body_type" class="px-4 py-2 rounded-lg border-none bg-gray-300 text-gray-800 focus:outline-none focus:bg-white focus:ring focus:ring-blue-500">
-                            <option value="Hatchback">Hatchback</option>
-                            <option value="Coupe">Coupe</option>
-                            <option value="SUV">SUV</option>
-                            <option value="Sedan">Sedan</option>
-                            <option value="Station wagon">Station wagon</option>
-                            <option value="Convertible">Convertible</option>
-                        </select>
-                            <label class="text-gray-400">Fuel type</label>
+                            <label class="text-black-400">Body type</label>
+                            <select required name="body_type" class="px-4 py-2 rounded-lg border-none bg-gray-300 text-gray-800 focus:outline-none focus:bg-white focus:ring focus:ring-blue-500">
+                                <option value="Hatchback">Hatchback</option>
+                                <option value="Coupe">Coupe</option>
+                                <option value="SUV">SUV</option>
+                                <option value="Sedan">Sedan</option>
+                                <option value="Station wagon">Station wagon</option>
+                                <option value="Convertible">Convertible</option>
+                            </select>
+                            <label class="text-black-400">Fuel type</label>
                             <select required name="fuel_type" class="px-4 py-2 rounded-lg border-none bg-gray-300 text-gray-800 focus:outline-none focus:bg-white focus:ring focus:ring-blue-500">
                                 <option value="Petrol">Petrol</option>
                                 <option value="Diesel">Diesel</option>
@@ -88,7 +87,7 @@
                             </select>
                         </div>
                         <div class="flex flex-col space-y-2 mx-4">
-                            <label class="text-gray-400">Power</label>
+                            <label class="text-black-400">Power</label>
                             <select required name="power" class="px-4 py-2 rounded-lg border-none bg-gray-300 text-gray-800 focus:outline-none focus:bg-white focus:ring focus:ring-blue-500">
                                 <option value="34">25KW (34KS)</option>
                                 <option value="48">35KW (48KS)</option>
@@ -105,20 +104,27 @@
                                 <option value="250">184KW (250KS)</option>
 
                             </select>
+
+                            <label class="text-black-400">Gear</label>
+                            <select required name="gear" class="px-4 py-2 rounded-lg border-none bg-gray-300 text-gray-800 focus:outline-none focus:bg-white focus:ring focus:ring-blue-500">
+                                <option value="Manual 4 gears">Manual 4 gears</option>
+                                <option value="Manual 5 gears">Manual 5 gears</option>
+                                <option value="Manual 6 gears">Manual 6 gears</option>
+                                <option value="Automatic">Automatic</option>
+                            </select>
+
+                            <label class="text-black-400">Number of doors</label>
+                            <select required name="number_of_doors" class="px-4 py-2 rounded-lg border-none bg-gray-300 text-gray-800 focus:outline-none focus:bg-white focus:ring focus:ring-blue-500">
+                                <option value="2">2/3</option>
+                                <option value="5">4/5</option>
+
+                            </select>
+                            <div>
+                                <label for="description" class="">Description</label>
+                                <textarea name="description" class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Description"></textarea>
+                            </div>
                         </div>
-                        <div>
-                            <label for="phone" class="">Gear</label>
-                            <input id="phone" name="gear" type="text"  class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Phone number" value="">
-                        </div>
-                        <div>
-                            <label for="phone" class="">Number of doors</label>
-                            <input id="phone" name="number_of_doors" type="number"  class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Phone number" value="">
-                        </div>
-                        <div>
-                            <label for="phone" class="">Description</label>
-                            <input id="phone" name="description" type="text"  class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Phone number" value="">
-                        </div>
-                        <input id="phone" name="user_car_id" type="number"  class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Phone number" value="{{auth()->user()->id}}">
+                        <input  id="phone" name="user_car_id" type="hidden"  class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Phone number" value="{{auth()->user()->id}}">
                         @if ($errors->any())
                             <div class="alert alert-danger text-red">
                                 <ul>
@@ -134,7 +140,7 @@
 
                     <div class="flex items-center justify-center">
                         <button  type="submit" class="group relative flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                            Update profile
+                            Add your car
                         </button>
                     </div>
 
