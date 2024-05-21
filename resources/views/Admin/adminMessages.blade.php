5c291a88aea1b65c26b4d2d4e398dd84a9b0b574
Nemuszz <nsavic3011@gmail.com>
2024-05-21 20:27:04 +0200

    added layout for admin navigation

--- resources/views/Admin/adminMessages.blade.php
+++ resources/views/Admin/adminMessages.blade.php
@@ -1,17 +1,7 @@
 @include('Layouts.nav')
 <div class="mx-auto max-w-screen-xl min-h-screen bg-white p-4 md:p-8 rounded-lg shadow-md">
     <!-- Main Content -->
-    <div class="flex flex-col">
-        <!-- Navigation -->
-        <div class="bg-gray-800 text-white flex justify-between items-center p-4">
-            <span class="text-xl md:text-2xl font-semibold">Admin</span>
-            <nav class=" flex">
-                <a href="{{route('admin.page')}}" class="p-2 md:p-4 hover:bg-gray-700">Car requests</a>
-                <a href="{{route('admin.users')}}" class="p-2 md:p-4 hover:bg-gray-700">Users</a>
-                <a href="" class="p-2 md:p-4 hover:bg-gray-700">Orders</a>
-                <a href="{{route('admin.messages')}}" class="p-2 md:p-4 hover:bg-gray-700">Messages</a>
-            </nav>
-        </div>
+    @include('Layouts.adminNav')
         <!-- Table -->
         <div class="flex-1">
 
