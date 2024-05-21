ad2471c7efb96d1c0e80d1b26e500197b46d7279
Nemuszz <nsavic3011@gmail.com>
2024-05-21 20:31:30 +0200

    added layout for user navigation

---
+++ resources/views/Layouts/userNav.blade.php
@@ -0,0 +1,14 @@
+<div class="bg-gray-800 text-white w-64 h-50 flex flex-col">
+    <div class="p-4 flex justify-between items-center">
+        <span class="text-2xl font-semibold">Dashboard</span>
+
+    </div>
+    <nav class="flex-1 overflow-y-auto">
+        <a href="{{route('user.profile', ['id'=> auth()->user()->id])}}" class="block p-4 hover:bg-gray-700">Profile</a>
+        <a href="{{route('car.add')}}" class="block p-4 hover:bg-gray-700">Add car</a>
+        <a href="{{route('car.yours', ['id'=> auth()->user()->id])}}" class="block p-4 hover:bg-gray-700">Your car</a>
+        <a href="#" class="block p-4 hover:bg-gray-700">Purchased parts</a>
+        <a href="{{route('user.contact', ['id'=> auth()->user()->id])}}" class="block p-4 hover:bg-gray-700">Contact</a>
+    </nav>
+
+</div>
--- resources/views/Pages/Profile.blade.php
+++ resources/views/Pages/Profile.blade.php
@@ -2,20 +2,7 @@
 <div class=" mx-auto max-w-screen-xl bg-white p-8 rounded-lg shadow-md">
 <div class="flex h-screen">
     <!-- Sidebar -->
-    <div class="bg-gray-800 text-white w-64 h-50 flex flex-col">
-        <div class="p-4 flex justify-between items-center">
-            <span class="text-2xl font-semibold">Dashboard</span>
-
-        </div>
-        <nav class="flex-1 overflow-y-auto">
-            <a href="{{route('user.profile', ['id'=> auth()->user()->id])}}" class="block p-4 hover:bg-gray-700">Profile</a>
-            <a href="{{route('car.add')}}" class="block p-4 hover:bg-gray-700">Add car</a>
-            <a href="{{route('car.yours', ['id'=> auth()->user()->id])}}" class="block p-4 hover:bg-gray-700">Your car</a>
-            <a href="#" class="block p-4 hover:bg-gray-700">Purchased parts</a>
-            <a href="{{route('user.contact', ['id'=> auth()->user()->id])}}" class="block p-4 hover:bg-gray-700">Contact</a>
-        </nav>
-
-    </div>
+    @include('Layouts.userNav')
     <!-- Main Content -->
     <div class="flex-1 p-10 ">
         <div class="w-15 mx-auto bg-white rounded-lg shadow-md p-8">
