ab656d795eb5ec81c0cd4397c60ad30a40f6c70a
Nemuszz <nsavic3011@gmail.com>
2024-05-22 10:42:05 +0200

    added margin top to button

--- resources/views/Admin/adminParts.blade.php
+++ resources/views/Admin/adminParts.blade.php
@@ -56,12 +56,12 @@
                             <label for="price" class="">Price</label>
                             <input id="price" name="price" type="number" class="px-4 py-2 rounded-lg border-2 border-black bg-white text-gray-800 focus:outline-none focus:bg-white focus:ring focus:ring-blue-500" placeholder="Price in euros" >
                         </div>
-                        <div>
+                        <div class="">
                             <label for="description" class="">Description</label>
                             <textarea name="description" class="appearance-none rounded-none relative block w-full px-3 py-2 border-2 border-black bg-white placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Description"></textarea>
                         </div>
-                        <div class="flex items-center justify-center">
-                            <button  type="submit" class="group relative flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                        <div class="flex items-center justify-center ">
+                            <button  type="submit" class="group relative flex justify-center py-2 px-4 mt-3 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
                                 Add part
                             </button>
                         </div>
