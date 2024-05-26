71a29d1577c4f2ba989231fae672fe8c7f9049a6
Nemuszz <nsavic3011@gmail.com>
2024-05-26 18:00:48 +0200

    optimized web.php

--- routes/web.php
+++ routes/web.php
@@ -10,41 +10,22 @@ use App\Http\Middleware\AuthMiddleware;
 use Illuminate\Support\Facades\Route;
 
 
-Route::get('/', function () {
-    return view('Guest/welcome');
+Route::get('/', function () {return view('Guest/welcome');
 });
-Route::get('/parts', function () {
-    return view('Guest/parts');
+Route::get('/parts', function () {return view('Guest/parts');
 });
-Route::get('/register', function () {
-    return view('Registration/register');
+Route::get('/register', function () {return view('Registration/register');
 });
-Route::get('/login', function () {
-    return view('Registration/login');
+Route::get('/login', function () {return view('Registration/login');
 });
 
 
-Route::get('/cars',[CarController::class, 'index'])->name('cars');
-Route::get('/search',[CarController::class, 'search'])->name('search');
-
-Route::controller(PartsController::class)->middleware(AuthMiddleware::class)->prefix('/parts')->group(function () {
-    Route::get('/', 'parts')->name('parts');
-    Route::get('/search', 'partSearch')->name('parts.search');
-    Route::get('/delete/{part}', 'partDelete')->name('parts.delete');
-    Route::post('/amount/{part}', 'partAmount')->name('parts.amount.insert');
-    Route::get('/permalink/{part}','partPermalink')->name('parts.permalink');
-
-});
-Route::post('/cart/add',[CartController::class, 'addToCart'])->name('cart.add');
-Route::get('/cart/remove/{partId}',[CartController::class, 'removeToCart'])->name('cart.remove');
-Route::post('/cart/purchase',[CartController::class, 'purchaseToCart'])->name('cart.purchase');
 
 Route::controller(UserController::class)->prefix('/user')->group(function () {
-   Route::post('/login', 'login')->name('user.login');
-   Route::post('/register', 'register')->name('user.register');
+    Route::post('/login', 'login')->name('user.login');
+    Route::post('/register', 'register')->name('user.register');
 
 });
-
 
 Route::controller(UserController::class)->middleware(AuthMiddleware::class)->prefix('/user')->group(function () {
     Route::get('/logout', 'logout')->name('user.logout');
@@ -53,18 +34,6 @@ Route::controller(UserController::class)->middleware(AuthMiddleware::class)->pre
     Route::get('/contact/{id}', 'contact')->name('user.contact');
     Route::post('/message/{id}', 'message')->name('user.message');
     Route::get('/cart', 'userCart')->name('user.cart');
-});
-
-
-Route::controller(CarController::class)->middleware(AuthMiddleware::class)->prefix('/car')->group(function () {
-    Route::get('/add', 'add')->name('car.add');
-    Route::post('/add/user_car', 'insert')->name('car.insert');
-    Route::get('/your/{id}','yours')->name('car.yours');
-    Route::get('/your/permalink/{car}','permalink')->name('car.permalink');
-    Route::get('/your/delete/{car}','delete')->name('car.delete');
-    Route::get('/your/change/{car}','changeCar')->name('car.change');
-    Route::post('/your/update/{car}','update')->name('car.update');
-
 });
 
 Route::controller(AdminController::class)->middleware([AdminMiddleware::class])->prefix('/admin')->group(function () {
@@ -78,5 +47,40 @@ Route::controller(AdminController::class)->middleware([AdminMiddleware::class])-
     Route::post('/parts/insert','adminPartsInsert')->name('admin.parts.insert');
     Route::get('/orders','adminOrders')->name('admin.orders');
 
+});
+
+
+Route::get('/cars',[CarController::class, 'index'])->name('cars');
+Route::get('/search',[CarController::class, 'search'])->name('search');
+
+Route::controller(CarController::class)->middleware(AuthMiddleware::class)->prefix('/car')->group(function () {
+    Route::get('/add', 'add')->name('car.add');
+    Route::post('/add/user_car', 'insert')->name('car.insert');
+    Route::get('/your/{id}','yours')->name('car.yours');
+    Route::get('/your/permalink/{car}','permalink')->name('car.permalink');
+    Route::get('/your/delete/{car}','delete')->name('car.delete');
+    Route::get('/your/change/{car}','changeCar')->name('car.change');
+    Route::post('/your/update/{car}','update')->name('car.update');
+});
+
+
+Route::controller(PartsController::class)->middleware(AuthMiddleware::class)->prefix('/parts')->group(function () {
+    Route::get('/', 'parts')->name('parts');
+    Route::get('/search', 'partSearch')->name('parts.search');
+    Route::get('/delete/{part}', 'partDelete')->name('parts.delete');
+    Route::post('/amount/{part}', 'partAmount')->name('parts.amount.insert');
+    Route::get('/permalink/{part}','partPermalink')->name('parts.permalink');
 
 });
+Route::controller(CartController::class)->middleware(AuthMiddleware::class)->prefix('/cart')->group(function () {
+    Route::post('/cart/add', 'addToCart')->name('cart.add');
+    Route::get('/cart/remove/{partId}', 'removeToCart')->name('cart.remove');
+    Route::post('/cart/purchase', 'purchaseToCart')->name('cart.purchase');
+
+});
+
+
+
+
+
+
