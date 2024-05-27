8131fa7b4d1413abfe98574ca497d08d9db3de1f
Nemuszz <nsavic3011@gmail.com>
2024-05-27 17:26:53 +0200

    added wish list front

---
+++ app/Models/WishListModel.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class WishListModel extends Model
+{
+    use HasFactory;
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function part()
+    {
+        return $this->belongsTo(PartsModel::class); // Assuming you have an Item model
+    }
+
+    protected $table = 'wishList';
+    protected $fillable = [
+        'user_id',
+        'part_id'
+
+    ];
+
+
+}
